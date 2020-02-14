698a796c9782f657c605af90471666de20193ad3
MarkMuniu <nyttocreator@gmail.com>
2020-02-14 17:15:53 +0300

    create variables for male and female akan names

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-var personName = document.getElementById("name").nodeValue;
-var personBirthday = document.getElementById("birthday").nodeValue;
-
+function submitInfo(){
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var personName = document.getElementById("name").value;
+var personBirthday = document.getElementById("birthday").value;
+console.log(personBirthday);
+ 
+if (document.getElementById("hisDate").checked){
+  ("Sunday" === "Kwasi")
+}
+if (document.getElementById("hisDate").checked){
+  ("Monday" === "Kwadwo")
+}
+if (document.getElementById("hisDate").checked){
+   ("Tuesday" === "Kwabena")
+}
