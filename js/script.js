8139ca154c9a7e5bbe86a7c483bdb6045b98587c
MarkMuniu <nyttocreator@gmail.com>
2020-02-15 19:01:04 +0300

    introduce a function that rounds off the result of the equation

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ function submitInfo(){
   var YY = parseInt(year.slice(2,4));
   var CC = parseInt(year.slice(0,2));
   var pickPosition = Math.abs(((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)10)+DD)%7;
-  pickPosition = Math.floor(pickPosition)
+  pickPosition = Math.floor(pickPosition);
   var personName = document.getElementById("name").value;
   var personBirthday = document.getElementById("birthday").value;
 
