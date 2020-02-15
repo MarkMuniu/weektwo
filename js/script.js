2d05c33b430428605138438d50aab8d60575e926
MarkMuniu <nyttocreator@gmail.com>
2020-02-15 18:24:55 +0300

    add value to the YY variable in the javascript formula

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function submitInfo(){
   var DD = parseInt(document.getElementById("day").value);
   var MM = parseInt(document.getElementById("month").value);
   var year = document.getElementById("year").value;
-  var YY = parseInt(year.slice(2,4))
+  var YY = parseInt(year.slice(2,4));
   var CC = parseInt(year.slice(0,2))
   var personName = document.getElementById("name").value;
   var personBirthday = document.getElementById("birthday").value;
