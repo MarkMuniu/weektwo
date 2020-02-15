bcf515484812ead3a7071f6756e3bdfa82fd8b98
MarkMuniu <nyttocreator@gmail.com>
2020-02-15 18:18:05 +0300

    add value to MM variable in the javascript formula

--- js/script.js
+++ js/script.js
@@ -1,17 +1,13 @@
 function submitInfo(){
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-var personName = document.getElementById("name").value;
-var personBirthday = document.getElementById("birthday").value;
-console.log(personBirthday);
- r
-if (document.getElementById("hisDate").checked){
-  ("Sunday" === "Kwasi")
-}
-if (document.getElementById("hisDate").checked){
-  ("Monday" === "Kwadwo")
-}
-if (document.getElementById("hisDate").checked){
-   ("Tuesday" === "Kwabena")
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var DD = parseInt(document.getElementById("day").value);
+  var MM = parseInt(document.getElementById("month").value);
+  var year = document.getElementById("year").value
+  var YY = parseInt(year.slice(2,4))
+  var CC = parseInt(year.slice(0,2))
+  var personName = document.getElementById("name").value;
+  var personBirthday = document.getElementById("birthday").value;
+
 }
