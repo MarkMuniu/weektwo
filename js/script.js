116c2d32342c4d1b6dc85a2621687c1a98977d69
MarkMuniu <nyttocreator@gmail.com>
2020-02-15 19:30:22 +0300

    introduce a control flow that will issue answers based on gender

--- js/script.js
+++ js/script.js
@@ -7,8 +7,15 @@ function submitInfo(){
   var year = document.getElementById("year").value;
   var YY = parseInt(year.slice(2,4));
   var CC = parseInt(year.slice(0,2));
-  var pickPosition = Math.abs(((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)10)+DD)%7;
+  var pickPosition = Math.abs(((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10)+DD)%7);
   pickPosition = Math.floor(pickPosition);
+  if(document.getElementById("hisgender").checked){
+    var gender = 'male';
+  }else{
+    var gender = 'female';
+  };
+  var name = sex[pickPosition]
+  days = 
   var personName = document.getElementById("name").value;
   var personBirthday = document.getElementById("birthday").value;
 
